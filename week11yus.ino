4bb416f8d2324a83e5440742ce4f0c32e0a440ec
tsaiyuyes7 <39110981+tsaiyuyes7@users.noreply.github.com>
2018-05-09 11:47:47 +0800

    Add files via upload

---
+++ week11yus.ino
@@ -0,0 +1,20 @@
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+
+void loop() {
+  digitalWrite(13, HIGH);   
+  delay(200);        
+   digitalWrite(13, LOW);   
+  delay(200);                               
+  digitalWrite(12, HIGH); 
+   delay(200);  
+  digitalWrite(12, LOW);    
+  delay(200);  
+  
+}
